48bd649f95d11b941e00018b3458a6329eafcf61
Jonathan Sick <jsick@lsst.org>
2017-07-21 16:09:48 -0400

    Upgrade to documenteer 0.2

--- conf.py
+++ conf.py
@@ -4,16 +4,16 @@
 # see metadata.yaml in this repo for to update document-specific metadata
 
 import os
-from documenteer.designdocs import configure_sphinx_design_doc
+from documenteer.sphinxconfig.technoteconf import configure_technote
 
 # Ingest settings from metadata.yaml and use documenteer's
 # configure_sphinx_design_doc to build a Sphinx configuration that is
 # injected into this script's global namespace.
 metadata_path = os.path.join(os.path.dirname(__file__), 'metadata.yaml')
 with open(metadata_path, 'r') as f:
-    confs = configure_sphinx_design_doc(f)
+    confs = configure_technote(f)
 g = globals()
 g.update(confs)
 
-# ingnore technote partials
+# ignore technote partials
 exclude_patterns.append('_*.rst')
